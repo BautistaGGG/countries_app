06265856667c55b3bdbb030e20d5ea66c88cd788
Bautista González Gastó <gonzalezbautistagasto@gmail.com>
2024-05-03 13:45:00 -0300

    Primeros pasos

---
+++ src/componentes/HomePage.jsx
@@ -0,0 +1,101 @@
+import { useEffect, useState } from 'react';
+
+function HomePage() {
+  const [countries, setCountries] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [selectedRegion, setSelectedRegion] = useState('');
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch('https://restcountries.com/v3.1/all');
+      const data = await response.json();
+      setCountries(data);
+    }
+    fetchData();
+  }, []);
+
+  function filterCountries() {
+    return countries.filter(country => {
+      const nameMatches = country.name.common.toLowerCase().includes(searchQuery.toLowerCase());
+      const regionMatches = !selectedRegion || country.region === selectedRegion;
+      return nameMatches && regionMatches;
+    });
+  }
+
+  function onChangeBusqueda(e) {
+    setSearchQuery(e.target.value)
+  }
+
+  function onChangeRegion(e) {
+    setSelectedRegion(e.target.value)
+  }
+
+  return (
+    <main className='container mx-auto'>
+      <section className='flex justify-between my-8'>
+        <input 
+          type="text" value={searchQuery} 
+          onChange={onChangeBusqueda} 
+          placeholder="Search for a country..." 
+          className='py-3 pl-4 pr-40 border border-slate-300'
+        />
+        <select 
+          value={selectedRegion} 
+          onChange={onChangeRegion}
+          className='py-3 px-5 shadow-lg'
+        >
+          <option value="">
+            All regions
+          </option>
+          <option value="Africa">
+            Africa
+          </option>
+          <option value="Americas">
+            Americas
+          </option>
+          <option value="Asia">
+            Asia
+          </option>
+          <option value="Europe">
+            Europe
+          </option>
+          <option value="Oceania">
+            Oceania
+          </option>
+        </select>
+      </section>
+
+      <ul className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-12 px-4 md:px-0 lg:px-0'>
+        {filterCountries().map(country => (
+          <li key={country.cca3} className='shadow-lg rounded-lg'>
+            <a href={`/country/${country.cca3}`}>
+              <img 
+                src={country.flags.svg} 
+                alt={country.name.common + "flag"} 
+              />
+              <div className='p-4'>
+                <h2 className='text-2xl font-bold'>
+                  {country.name.common}
+                </h2>
+                <p className='mb-2'>
+                  <span className='font-bold'>Population: </span> 
+                  {country.population}
+                </p>
+                <p className='mb-2'>
+                  <span className='font-bold'>Region: </span> 
+                  {country.region}
+                </p>
+                <p className='mb-2'>
+                  <span className='font-bold'>Capital: </span> 
+                  {country.capital}
+                </p>
+              </div>
+            </a>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+}
+
+export default HomePage
---
+++ src/componentes/Navbar.jsx
@@ -0,0 +1,14 @@
+function Navbar() {
+  return (
+    <nav className='flex justify-between p-8 shadow-xl'>
+        <a href="/">
+            <h1 className="font-bold text-2xl">
+              Where in the world?
+            </h1>
+        </a>
+        <p>Dark mode</p>
+    </nav>
+  )
+}
+
+export default Navbar
